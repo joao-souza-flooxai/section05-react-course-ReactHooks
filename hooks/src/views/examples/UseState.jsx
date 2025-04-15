928796f4799e2fed4236a128b4568f26d716fb0a
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-15 11:01:59 -0300

    feat: Added useState example.

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+import SectionTitle from "../../components/layout/SectionTitle"
 const UseState = (props) => {
+
+    /*
+        Usa a desestruturação para criar a variavel que será alterada
+        (count) quem irá alterada-la(setCount) e qual é o valor inicial(0).
+    */
+    const [count, setCount] = useState(0);
+    const[name, setName] = useState("");
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+        <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                <button className="btn" 
+                    onClick={()=>setCount(count -1)}>
+                    -1</button>
+                <button className="btn"  
+                    onClick={()=>setCount(count +1)}>
+                    +1</button>
+                    <button 
+                        className="btn"  
+                        onClick={() => setCount(current => {
+                            alert(current);
+                            return current;
+                        })}
+                        >
+                        Valor atual
+                    </button>
+                </div>
+            </div>
+        <SectionTitle title="Exercício #02"/>
+            <input type="text" className="input" 
+            value={name} placeholder='Type something...' onChange={e=>{
+                    setName(e.target.value)     
+                }}/> 
+
+                <span className="text">Value typed: {name}</span>           
         </div>
     )
 }
