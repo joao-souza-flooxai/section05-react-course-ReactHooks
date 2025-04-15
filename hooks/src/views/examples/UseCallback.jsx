7da70998e8912a39033b31e3b51e7651475aaee1
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-15 13:42:14 -0300

    feat: Added useCallBack example.

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './useCallbackButtons';
 
 const UseCallback = (props) => {
+
+    const [count, setCount] = useState(0);
+
+    //usando a mesma função para rendenizar o componente evitando a rendenização desnecessária do UserCallBackButtons
+    const increment = useCallback(function (delta) {
+        setCount(curr => curr + delta);
+    }, [setCount]);
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
+                subtitle="Retorna uma função memorizada!" 
             />
+
+            <div className="center">
+                <span className="text">{count}</span>
+
+               <UseCallbackButtons inc={increment}/>
+            </div>
         </div>
-    )
+    );
 }
 
 export default UseCallback
